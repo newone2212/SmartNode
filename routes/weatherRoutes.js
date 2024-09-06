590a6dfd1536103c89716f347910ed2d8484478a
Parth Chaturvedi <153600672+newone2212@users.noreply.github.com>
2024-09-06 07:42:38 +0530

    Update weatherRoutes.js

--- routes/weatherRoutes.js
+++ routes/weatherRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { addLocation, getSunriseSunset } = require('../controller/weatherController');
+const { addLocation, getSunriseSunset,greetingUser } = require('../controller/weatherController');
 const router = express.Router();
 
 //route to add location
 router.post('/location', addLocation);
 //route to get location data
 router.get('/location/:lat/:lon', getSunriseSunset);
+//route for greatting
+router.get('/Greet/:lat/:lon', greetingUser);
 
 module.exports = router;
